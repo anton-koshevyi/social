90166ab476171823c3f26907cc58837105a2d07c
Anton Koshovyi <anton.koshovyi.99@gmail.com>
2020-07-18 23:55:40 +0300

    Implement entity

---
+++ backend/src/main/java/com/social/backend/model/post/Post.java
@@ -0,0 +1,102 @@
+package com.social.backend.model.post;
+
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.social.backend.model.user.User;
+
+@Entity
+@Table(name = "posts")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    
+    @Column(name = "creation_milli", nullable = false)
+    private Long creationMilli;
+    
+    @Column(name = "body", nullable = false)
+    private String body;
+    
+    @OneToMany(mappedBy = "post")
+    private List<Comment> comments;
+    
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User author;
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Post post = (Post) o;
+        return Objects.equals(creationMilli, post.creationMilli)
+                && Objects.equals(body, post.body)
+                && Objects.equals(comments, post.comments)
+                && Objects.equals(author, post.author);
+    }
+    
+    @Override
+    public int hashCode() {
+        return Objects.hash(creationMilli, body, comments, author);
+    }
+    
+    public Post setId(Long id) {
+        this.id = id;
+        return this;
+    }
+    
+    public Post setCreationMilli(Long creationMilli) {
+        this.creationMilli = creationMilli;
+        return this;
+    }
+    
+    public Post setBody(String body) {
+        this.body = body;
+        return this;
+    }
+    
+    public Post setComments(List<Comment> comments) {
+        this.comments = comments;
+        return this;
+    }
+    
+    public Post setAuthor(User author) {
+        this.author = author;
+        return this;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public Long getCreationMilli() {
+        return creationMilli;
+    }
+    
+    public String getBody() {
+        return body;
+    }
+    
+    public List<Comment> getComments() {
+        return comments;
+    }
+    
+    public User getAuthor() {
+        return author;
+    }
+}
---
+++ backend/src/main/java/com/social/backend/model/user/Publicity.java
@@ -0,0 +1,9 @@
+package com.social.backend.model.user;
+
+public final class Publicity {
+    public static final int PUBLIC = 30;
+    public static final int INTERNAL = 20;
+    public static final int PRIVATE = 10;
+    
+    private Publicity() {}
+}
