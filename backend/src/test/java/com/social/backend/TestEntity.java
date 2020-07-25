65cf1123d134ff3c9e806003c0b1d8e4beda0ad7
Anton Koshovyi <anton.koshovyi.99@gmail.com>
2020-07-25 16:42:59 +0300

    Add methods for chat entities

--- backend/src/test/java/com/social/backend/TestEntity.java
+++ backend/src/test/java/com/social/backend/TestEntity.java
@@ -1,5 +1,7 @@
 package com.social.backend;
 
+import com.social.backend.model.chat.GroupChat;
+import com.social.backend.model.chat.PrivateChat;
 import com.social.backend.model.post.Comment;
 import com.social.backend.model.post.Post;
 import com.social.backend.model.user.User;
@@ -23,6 +25,15 @@ public final class TestEntity {
     public static Comment comment() {
         return new Comment()
                 .setBody("comment body");
+    }
+    
+    public static PrivateChat privateChat() {
+        return new PrivateChat();
+    }
+    
+    public static GroupChat groupChat() {
+        return new GroupChat()
+                .setName("name");
     }
     
     private TestEntity() {}
