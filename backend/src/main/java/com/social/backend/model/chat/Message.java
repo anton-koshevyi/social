59c2e7fab4cf1b3c2a2b9eac3ecdf5b713fd6408
Anton Koshovyi <anton.koshovyi.99@gmail.com>
2020-07-26 21:45:54 +0300

    Implement base Reply entity

---
+++ backend/src/main/java/com/social/backend/model/chat/Message.java
@@ -0,0 +1,25 @@
+package com.social.backend.model.chat;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.social.backend.model.Reply;
+
+@Entity
+@Table(name = "messages")
+public class Message extends Reply {
+    @ManyToOne
+    @JoinColumn(name = "chat_id")
+    private Chat chat;
+    
+    public Message setChat(Chat chat) {
+        this.chat = chat;
+        return this;
+    }
+    
+    public Chat getChat() {
+        return chat;
+    }
+}
