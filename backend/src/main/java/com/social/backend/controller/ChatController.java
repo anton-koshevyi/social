ef99ee8e097b1c7b20b0987a4f8dd78b836692ae
Anton Koshovyi <anton.koshovyi.99@gmail.com>
2020-07-28 19:21:05 +0300

    Implement 'setOwner' method

--- backend/src/main/java/com/social/backend/controller/ChatController.java
+++ backend/src/main/java/com/social/backend/controller/ChatController.java
@@ -1,6 +1,7 @@
 package com.social.backend.controller;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import javax.validation.Valid;
 
@@ -16,7 +17,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.social.backend.dto.chat.GroupDto;
+import com.social.backend.dto.chat.CreateGroupDto;
+import com.social.backend.dto.chat.UpdateGroupDto;
+import com.social.backend.dto.chat.UpdateGroupMembersDto;
 import com.social.backend.model.chat.Chat;
 import com.social.backend.model.user.User;
 import com.social.backend.service.ChatService;
@@ -36,15 +39,15 @@ public class ChatController {
     @GetMapping("/chats")
     public Page<Chat> userAll(@AuthenticationPrincipal(expression = "id") Long userId,
                               Pageable pageable) {
-        User user = userService.findById(userId);
-        return chatService.findAllByUser(user, pageable);
+        User member = userService.findById(userId);
+        return chatService.findAllByMember(member, pageable);
     }
     
     @GetMapping("/chats/{id}")
     public Chat chatByIdAndUser(@PathVariable Long id,
                                 @AuthenticationPrincipal(expression = "id") Long userId) {
-        User user = userService.findById(userId);
-        return chatService.findByIdAndUser(id, user);
+        User member = userService.findById(userId);
+        return chatService.findByIdAndMember(id, member);
     }
     
     @GetMapping("/chats/{id}/members")
@@ -55,7 +58,7 @@ public class ChatController {
         return chatService.getMembers(id, member, pageable);
     }
     
-    @DeleteMapping("/chats/{id}/private")
+    @DeleteMapping("/chats/private/{id}")
     public void deletePrivate(@PathVariable("id") Long id,
                               @AuthenticationPrincipal(expression = "id") Long userId) {
         User user = userService.findById(userId);
@@ -64,58 +67,57 @@ public class ChatController {
     
     @PostMapping("/chats/group")
     public Chat groupChat(@AuthenticationPrincipal(expression = "id") Long userId,
-                          @Valid @RequestBody GroupDto dto) {
+                          @Valid @RequestBody CreateGroupDto dto) {
         User creator = userService.findById(userId);
         String name = dto.getName();
-        List<User> members = dto.getMemberIds()
-                .stream()
-                .map(userService::findById)
-                .collect(Collectors.toList());
+        Set<User> members = findUsersByIds(dto.getMemberIds());
         return chatService.createGroup(creator, name, members);
     }
     
-    @PutMapping("/chats/{id}/group")
-    public Chat updateGroup(@PathVariable Long id,
-                            @AuthenticationPrincipal(expression = "id") Long userId,
-                            @Valid @RequestBody GroupDto dto) {
+    @PostMapping("/chats/group/{id}")
+    public Chat updatedGroup(@PathVariable Long id,
+                             @AuthenticationPrincipal(expression = "id") Long userId,
+                             @Valid @RequestBody UpdateGroupDto dto) {
         User member = userService.findById(userId);
         String name = dto.getName();
-        List<User> newMembers = dto.getMemberIds()
-                .stream()
-                .map(userService::findById)
-                .collect(Collectors.toList());
-        return chatService.updateGroup(id, member, name, newMembers);
+        return chatService.updateGroup(id, member, name);
     }
     
-    @DeleteMapping("/chats/{id}/group")
-    public void deleteGroup(@PathVariable("id") Long id,
-                            @AuthenticationPrincipal(expression = "id") Long userId) {
-        chatService.deleteGroup(id, userId);
-    }
-    
-    // TODO: Set owner
-    
-    // @PutMapping("/chats/{id}/group/members/{userId}")
-    // public Chat newOwner(@PathVariable Long id,
-    //                         @AuthenticationPrincipal(expression = "id") Long userId,
-    //                         @Valid @RequestBody GroupDto dto) {
-    // }
-    
-    @PutMapping("/chats/{id}/group/members")
-    public Chat removeMembers(@PathVariable("id") Long id,
-                              @AuthenticationPrincipal(expression = "id") Long userId,
-                              @Valid @RequestBody GroupDto dto) {
-        List<User> members = dto.getMemberIds()
-                .stream()
-                .map(userService::findById)
-                .collect(Collectors.toList());
-        return chatService.removeGroupMembers(id, userId, members);
-    }
-    
-    @PutMapping("/chats/{id}/group/leave")
+    @PutMapping("/chats/group/{id}")
     public void leaveGroup(@PathVariable("id") Long id,
                            @AuthenticationPrincipal(expression = "id") Long userId) {
         User user = userService.findById(userId);
         chatService.leaveGroup(id, user);
+    }
+    
+    @DeleteMapping("/chats/group/{id}")
+    public void deleteGroup(@PathVariable("id") Long id,
+                            @AuthenticationPrincipal(expression = "id") Long userId) {
+        User owner = userService.findById(userId);
+        chatService.deleteGroup(id, owner);
+    }
+    
+    @PutMapping("/chats/group/{id}/members")
+    public Chat updatedGroupMembers(@PathVariable Long id,
+                                    @AuthenticationPrincipal(expression = "id") Long userId,
+                                    @Valid @RequestBody UpdateGroupMembersDto dto) {
+        User member = userService.findById(userId);
+        Set<User> members = findUsersByIds(dto.getMemberIds());
+        return chatService.updateGroupMembers(id, member, members);
+    }
+    
+    @PutMapping("/chats/group/{id}/members/{newOwnerId}")
+    public Chat newOwner(@PathVariable Long id,
+                         @AuthenticationPrincipal(expression = "id") Long userId,
+                         @PathVariable Long newOwnerId) {
+        User owner = userService.findById(userId);
+        User newOwner = userService.findById(newOwnerId);
+        return chatService.setOwner(id, owner, newOwner);
+    }
+    
+    private Set<User> findUsersByIds(List<Long> ids) {
+        return ids.stream()
+                .map(userService::findById)
+                .collect(Collectors.toSet());
     }
 }
