6b20b4dfbe4748269b130b047a493d8a98eb8083
Anton Koshovyi <anton.koshovyi.99@gmail.com>
2020-08-01 15:21:53 +0300

    Test non-administration request for public body

--- backend/src/main/java/com/social/backend/adapter/json/UserSerializer.java
+++ backend/src/main/java/com/social/backend/adapter/json/UserSerializer.java
@@ -2,7 +2,6 @@ package com.social.backend.adapter.json;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jackson.JsonComponent;
 import org.springframework.security.access.expression.SecurityExpressionRoot;
@@ -19,52 +18,59 @@ import com.social.backend.util.AuthenticationUtil;
 
 @JsonComponent
 public class UserSerializer extends AbstractSerializer<User> {
-    private final Logger logger = LoggerFactory.getLogger(UserSerializer.class);
-    private final EntityMapper<User, UserDto> entityMapper;
+  private final Logger logger = LoggerFactory.getLogger(UserSerializer.class);
+  private final EntityMapper<User, UserDto> entityMapper;
+  
+  @Autowired
+  public UserSerializer(EntityMapper<User, UserDto> entityMapper) {
+    this.entityMapper = entityMapper;
+  }
+  
+  @SuppressWarnings({"checkstyle:CyclomaticComplexity", "checkstyle:JavaNCSS"})
+  @Override
+  public Object beforeSerialize(User user) {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
     
-    @Autowired
-    public UserSerializer(EntityMapper<User, UserDto> entityMapper) {
-        this.entityMapper = entityMapper;
+    if (authentication == null) {
+      if (user.isPublic()) {
+        // Unable to check request from administration
+        logger.debug("Null authentication - regular 'public' body");
+        return entityMapper.map(user);
+      }
+      
+      logger.debug("Null authentication - hidden body");
+      return entityMapper.mapHidden(user);
     }
     
-    @SuppressWarnings({"checkstyle:CyclomaticComplexity", "checkstyle:JavaNCSS"})
-    @Override
-    public Object beforeSerialize(User user) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        
-        if (authentication == null) {
-            if (user.isPublic()) {
-                logger.debug("Regular body for all users");
-                return entityMapper.map(user);
-            }
-            
-            logger.debug("Hidden body for null authentication");
-            return entityMapper.mapHidden(user);
-        }
-        
-        IdentifiedUserDetails principal = AuthenticationUtil.getPrincipal(authentication);
-        
-        if (principal != null && principal.getId().equals(user.getId())) {
-            logger.debug("Regular body for owner");
-            return entityMapper.map(user);
-        }
-        
-        SecurityExpressionRoot security = new SecurityExpressionRoot(authentication) {};
-        security.setTrustResolver(new AuthenticationTrustResolverImpl());
-        
-        if (security.hasAnyAuthority(Authority.MODER, Authority.ADMIN)) {
-            // Administration role-based access logic will be here.
-            // E.g. Moder cannot get extended data of another moder.
-            logger.debug("Extended body for administration");
-            return entityMapper.mapExtended(user);
-        }
-        
-        if (user.isInternal() && security.isAuthenticated()) {
-            logger.debug("Regular body for authenticated");
-            return entityMapper.map(user);
-        }
-        
-        logger.debug("Hidden body by default");
-        return entityMapper.mapHidden(user);
+    SecurityExpressionRoot security = new SecurityExpressionRoot(authentication) {};
+    security.setTrustResolver(new AuthenticationTrustResolverImpl());
+    
+    if (security.hasAnyAuthority(Authority.MODER, Authority.ADMIN)) {
+      // Administration role-based access logic will be here.
+      // E.g. Moder cannot get extended data of another moder.
+      logger.debug("Administration principal - extended body");
+      return entityMapper.mapExtended(user);
     }
+    
+    IdentifiedUserDetails principal = AuthenticationUtil.getPrincipal(authentication);
+    
+    if (principal != null && principal.getId().equals(user.getId())) {
+      logger.debug("Owner principal - regular body");
+      return entityMapper.map(user);
+    }
+    
+    if (user.isPublic()) {
+      // Request is not from administration
+      logger.debug("Regular 'public' body");
+      return entityMapper.map(user);
+    }
+    
+    if (user.isInternal() && security.isAuthenticated()) {
+      logger.debug("Authenticated - regular 'internal' body");
+      return entityMapper.map(user);
+    }
+    
+    logger.debug("Hidden body by default");
+    return entityMapper.mapHidden(user);
+  }
 }
--- backend/src/test/java/com/social/backend/adapter/json/UserSerializerTest.java
+++ backend/src/test/java/com/social/backend/adapter/json/UserSerializerTest.java
@@ -3,13 +3,13 @@ package com.social.backend.adapter.json;
 import java.io.IOException;
 import java.util.Collections;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.skyscreamer.jsonassert.Customization;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.skyscreamer.jsonassert.comparator.CustomComparator;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.AbstractJsonMarshalTester;
@@ -24,221 +24,271 @@ import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 
+import com.social.backend.TestEntity;
 import com.social.backend.config.IdentifiedUserDetails;
 import com.social.backend.config.SecurityConfig.Authority;
 import com.social.backend.model.user.Publicity;
 import com.social.backend.model.user.User;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import static com.social.backend.TestEntity.user;
-
 @JsonTest
 @ActiveProfiles("test")
 @ComponentScan("com.social.backend.dto")
 public class UserSerializerTest {
-    @Autowired
-    private AbstractJsonMarshalTester<User> tester;
+  
+  @Autowired
+  private AbstractJsonMarshalTester<User> tester;
+  
+  @Test
+  public void given_notPublicPublicity_when_nullAuthentication_then_hiddenBody()
+      throws IOException {
+    User user = TestEntity
+        .user()
+        .setId(1L)
+        .setPublicity(Publicity.INTERNAL);
     
-    @Test
-    public void given_notPublicPublicity_when_nullAuthentication_then_hiddenBody() throws IOException {
-        User user = user()
-                .setId(1L)
-                .setPublicity(Publicity.INTERNAL);
-        
-        String expected = "{"
-                + "id: 1,"
-                + "username: 'username',"
-                + "firstName: 'first',"
-                + "lastName: 'last',"
-                + "publicity: 20,"
-                + "moder: false,"
-                + "admin: false"
-                + "}";
-        assertThat(tester.write(user))
-                .isEqualToJson(expected, JSONCompareMode.NON_EXTENSIBLE);
-    }
+    String expected = "{"
+        + "id: 1,"
+        + "username: 'username',"
+        + "firstName: 'first',"
+        + "lastName: 'last',"
+        + "publicity: 20,"
+        + "moder: false,"
+        + "admin: false"
+        + "}";
+    Assertions
+        .assertThat(tester.write(user))
+        .isEqualToJson(expected, JSONCompareMode.NON_EXTENSIBLE);
+  }
+  
+  @Test
+  public void given_publicPublicity_when_nullAuthentication_then_regularBody()
+      throws IOException {
+    User user = TestEntity
+        .user()
+        .setId(1L)
+        .setPublicity(Publicity.PUBLIC);
     
-    @Test
-    public void given_publicPublicity_when_anyRequest_then_regularBody() throws IOException {
-        User user = user()
-                .setId(1L)
-                .setPublicity(Publicity.PUBLIC);
-        
-        String expected = "{"
-                + "id: 1,"
-                + "email: 'email@mail.com',"
-                + "username: 'username',"
-                + "firstName: 'first',"
-                + "lastName: 'last',"
-                + "publicity: 30,"
-                + "moder: false,"
-                + "admin: false"
-                + "}";
-        assertThat(tester.write(user))
-                .isEqualToJson(expected, JSONCompareMode.NON_EXTENSIBLE);
-    }
+    String expected = "{"
+        + "id: 1,"
+        + "email: 'email@mail.com',"
+        + "username: 'username',"
+        + "firstName: 'first',"
+        + "lastName: 'last',"
+        + "publicity: 30,"
+        + "moder: false,"
+        + "admin: false"
+        + "}";
+    Assertions
+        .assertThat(tester.write(user))
+        .isEqualToJson(expected, JSONCompareMode.NON_EXTENSIBLE);
+  }
+  
+  @ParameterizedTest
+  @ValueSource(ints = {Publicity.PUBLIC, Publicity.INTERNAL, Publicity.PRIVATE})
+  @WithMockUser(authorities = Authority.MODER)
+  public void given_anyPublicity_when_administrationRequest_then_extendedBody(int publicity)
+      throws IOException {
+    User user = TestEntity
+        .user()
+        .setId(1L)
+        .setPublicity(publicity);
     
-    @Test
-    @WithMockUser
-    public void given_privatePublicity_when_notIdentifiedPrincipal_then_hiddenBodyByDefault() throws IOException {
-        User user = user()
-                .setId(1L)
-                .setPublicity(Publicity.PRIVATE);
-        
-        String expected = "{"
-                + "id: 1,"
-                + "username: 'username',"
-                + "firstName: 'first',"
-                + "lastName: 'last',"
-                + "publicity: 10,"
-                + "moder: false,"
-                + "admin: false"
-                + "}";
-        assertThat(tester.write(user))
-                .isEqualToJson(expected, JSONCompareMode.NON_EXTENSIBLE);
-    }
-    
-    @Test
-    public void given_privatePublicity_when_notOwnerRequest_then_hiddenBody() throws IOException {
-        authenticate(new IdentifiedUserDetails(
-                2L,
-                "username",
-                "password",
-                Collections.emptySet()
+    String expected = "{"
+        + "id: 1,"
+        + "email: 'email@mail.com',"
+        + "username: 'username',"
+        + "firstName: 'first',"
+        + "lastName: 'last',"
+        + "publicity: (customized),"
+        + "moder: false,"
+        + "admin: false"
+        + "}";
+    Assertions
+        .assertThat(tester.write(user))
+        .isEqualToJson(expected, new CustomComparator(JSONCompareMode.NON_EXTENSIBLE,
+            new Customization("publicity", (act, exp) -> true)
         ));
-        User user = user()
-                .setId(1L)
-                .setPublicity(Publicity.PRIVATE);
-        
-        String expected = "{"
-                + "id: 1,"
-                + "username: 'username',"
-                + "firstName: 'first',"
-                + "lastName: 'last',"
-                + "publicity: 10,"
-                + "moder: false,"
-                + "admin: false"
-                + "}";
-        assertThat(tester.write(user))
-                .isEqualToJson(expected, JSONCompareMode.NON_EXTENSIBLE);
-    }
+  }
+  
+  @Test
+  @WithMockUser
+  public void given_privatePublicity_when_notIdentifiedPrincipal_then_hiddenBody()
+      throws IOException {
+    User user = TestEntity
+        .user()
+        .setId(1L)
+        .setPublicity(Publicity.PRIVATE);
     
-    @Test
-    public void given_anyPublicity_when_ownerRequest_then_regularBody() throws IOException {
-        authenticate(new IdentifiedUserDetails(
-                1L,
-                "username",
-                "password",
-                Collections.emptySet()
-        ));
-        User user = user()
-                .setId(1L)
-                .setPublicity(Publicity.PRIVATE);
-        
-        String expected = "{"
-                + "id: 1,"
-                + "email: 'email@mail.com',"
-                + "username: 'username',"
-                + "firstName: 'first',"
-                + "lastName: 'last',"
-                + "publicity: 10,"
-                + "moder: false,"
-                + "admin: false"
-                + "}";
-        assertThat(tester.write(user))
-                .isEqualToJson(expected, JSONCompareMode.NON_EXTENSIBLE);
-    }
+    String expected = "{"
+        + "id: 1,"
+        + "username: 'username',"
+        + "firstName: 'first',"
+        + "lastName: 'last',"
+        + "publicity: 10,"
+        + "moder: false,"
+        + "admin: false"
+        + "}";
+    Assertions
+        .assertThat(tester.write(user))
+        .isEqualToJson(expected, JSONCompareMode.NON_EXTENSIBLE);
+  }
+  
+  @Test
+  public void given_privatePublicity_when_notOwnerRequest_then_hiddenBody()
+      throws IOException {
+    authenticate(new IdentifiedUserDetails(
+        2L,
+        "username",
+        "password",
+        Collections.emptySet()
+    ));
+    User user = TestEntity
+        .user()
+        .setId(1L)
+        .setPublicity(Publicity.PRIVATE);
     
-    @Test
-    @WithAnonymousUser
-    public void given_internalPublicity_when_anonymousRequest_then_hiddenBody() throws IOException {
-        User user = user()
-                .setId(1L)
-                .setPublicity(Publicity.INTERNAL);
-        
-        String expected = "{"
-                + "id: 1,"
-                + "username: 'username',"
-                + "firstName: 'first',"
-                + "lastName: 'last',"
-                + "publicity: 20,"
-                + "moder: false,"
-                + "admin: false"
-                + "}";
-        assertThat(tester.write(user))
-                .isEqualToJson(expected, JSONCompareMode.NON_EXTENSIBLE);
-    }
+    String expected = "{"
+        + "id: 1,"
+        + "username: 'username',"
+        + "firstName: 'first',"
+        + "lastName: 'last',"
+        + "publicity: 10,"
+        + "moder: false,"
+        + "admin: false"
+        + "}";
+    Assertions
+        .assertThat(tester.write(user))
+        .isEqualToJson(expected, JSONCompareMode.NON_EXTENSIBLE);
+  }
+  
+  @Test
+  public void given_anyPublicity_when_ownerRequest_then_regularBody()
+      throws IOException {
+    authenticate(new IdentifiedUserDetails(
+        1L,
+        "username",
+        "password",
+        Collections.emptySet()
+    ));
+    User user = TestEntity
+        .user()
+        .setId(1L)
+        .setPublicity(Publicity.PRIVATE);
     
-    @Test
-    @WithMockUser
-    public void given_internalPublicity_when_authenticatedRequest_then_regularBody() throws IOException {
-        User user = user()
-                .setId(1L)
-                .setPublicity(Publicity.INTERNAL);
-        
-        String expected = "{"
-                + "id: 1,"
-                + "email: 'email@mail.com',"
-                + "username: 'username',"
-                + "firstName: 'first',"
-                + "lastName: 'last',"
-                + "publicity: 20,"
-                + "moder: false,"
-                + "admin: false"
-                + "}";
-        assertThat(tester.write(user))
-                .isEqualToJson(expected, JSONCompareMode.NON_EXTENSIBLE);
-    }
+    String expected = "{"
+        + "id: 1,"
+        + "email: 'email@mail.com',"
+        + "username: 'username',"
+        + "firstName: 'first',"
+        + "lastName: 'last',"
+        + "publicity: 10,"
+        + "moder: false,"
+        + "admin: false"
+        + "}";
+    Assertions
+        .assertThat(tester.write(user))
+        .isEqualToJson(expected, JSONCompareMode.NON_EXTENSIBLE);
+  }
+  
+  @Test
+  @WithAnonymousUser
+  public void given_publicPublicity_when_notOwnerNorAdministrationRequest_then_regularBody()
+      throws IOException {
+    User user = TestEntity
+        .user()
+        .setId(1L)
+        .setPublicity(Publicity.PUBLIC);
     
-    @ParameterizedTest
-    @ValueSource(ints = {Publicity.PUBLIC, Publicity.INTERNAL, Publicity.PRIVATE})
-    @WithMockUser(authorities = Authority.MODER)
-    public void given_anyPublicity_when_administrationRequest_then_extendedBody(int publicity) throws IOException {
-        User user = user()
-                .setId(1L)
-                .setPublicity(publicity);
-        
-        String expected = "{"
-                + "id: 1,"
-                + "email: 'email@mail.com',"
-                + "username: 'username',"
-                + "firstName: 'first',"
-                + "lastName: 'last',"
-                + "publicity: (customized),"
-                + "moder: false,"
-                + "admin: false"
-                + "}";
-        assertThat(tester.write(user))
-                .isEqualToJson(expected, new CustomComparator(JSONCompareMode.NON_EXTENSIBLE,
-                        new Customization("publicity", (act, exp) -> true)
-                ));
-    }
+    String expected = "{"
+        + "id: 1,"
+        + "email: 'email@mail.com',"
+        + "username: 'username',"
+        + "firstName: 'first',"
+        + "lastName: 'last',"
+        + "publicity: 30,"
+        + "moder: false,"
+        + "admin: false"
+        + "}";
+    Assertions
+        .assertThat(tester.write(user))
+        .isEqualToJson(expected, JSONCompareMode.NON_EXTENSIBLE);
+  }
+  
+  @Test
+  @WithAnonymousUser
+  public void given_internalPublicity_when_anonymousRequest_then_hiddenBody()
+      throws IOException {
+    User user = TestEntity
+        .user()
+        .setId(1L)
+        .setPublicity(Publicity.INTERNAL);
     
-    @Test
-    @WithMockUser
-    public void given_privatePublicity_when_notOwnerNorAdministrationRequest_hiddenBody() throws IOException {
-        User user = user()
-                .setId(1L)
-                .setPublicity(Publicity.PRIVATE);
-        
-        String expected = "{"
-                + "id: 1,"
-                + "username: 'username',"
-                + "firstName: 'first',"
-                + "lastName: 'last',"
-                + "publicity: 10,"
-                + "moder: false,"
-                + "admin: false"
-                + "}";
-        assertThat(tester.write(user))
-                .isEqualToJson(expected, JSONCompareMode.NON_EXTENSIBLE);
-    }
+    String expected = "{"
+        + "id: 1,"
+        + "username: 'username',"
+        + "firstName: 'first',"
+        + "lastName: 'last',"
+        + "publicity: 20,"
+        + "moder: false,"
+        + "admin: false"
+        + "}";
+    Assertions
+        .assertThat(tester.write(user))
+        .isEqualToJson(expected, JSONCompareMode.NON_EXTENSIBLE);
+  }
+  
+  @Test
+  @WithMockUser
+  public void given_internalPublicity_when_authenticatedRequest_then_regularBody()
+      throws IOException {
+    User user = TestEntity
+        .user()
+        .setId(1L)
+        .setPublicity(Publicity.INTERNAL);
     
-    private static void authenticate(UserDetails details) {
-        AuthenticationProvider provider = new TestingAuthenticationProvider();
-        Authentication token = new TestingAuthenticationToken(details, details.getPassword());
-        Authentication authentication = provider.authenticate(token);
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-    }
+    String expected = "{"
+        + "id: 1,"
+        + "email: 'email@mail.com',"
+        + "username: 'username',"
+        + "firstName: 'first',"
+        + "lastName: 'last',"
+        + "publicity: 20,"
+        + "moder: false,"
+        + "admin: false"
+        + "}";
+    Assertions
+        .assertThat(tester.write(user))
+        .isEqualToJson(expected, JSONCompareMode.NON_EXTENSIBLE);
+  }
+  
+  @Test
+  @WithMockUser
+  public void given_privatePublicity_when_notOwnerNorAdministrationRequest_hiddenBody()
+      throws IOException {
+    User user = TestEntity
+        .user()
+        .setId(1L)
+        .setPublicity(Publicity.PRIVATE);
+    
+    String expected = "{"
+        + "id: 1,"
+        + "username: 'username',"
+        + "firstName: 'first',"
+        + "lastName: 'last',"
+        + "publicity: 10,"
+        + "moder: false,"
+        + "admin: false"
+        + "}";
+    Assertions
+        .assertThat(tester.write(user))
+        .isEqualToJson(expected, JSONCompareMode.NON_EXTENSIBLE);
+  }
+  
+  private static void authenticate(UserDetails details) {
+    AuthenticationProvider provider = new TestingAuthenticationProvider();
+    Authentication token = new TestingAuthenticationToken(details, details.getPassword());
+    Authentication authentication = provider.authenticate(token);
+    SecurityContextHolder.getContext().setAuthentication(authentication);
+  }
+  
 }
