6d8f5878a01e9677fa3168b3c94789279bf6bbe2
Anton Koshovyi <anton.koshovyi.99@gmail.com>
2020-08-05 20:19:48 +0300

    Use partial updates in corresponding methods

--- backend/src/main/java/com/social/backend/service/PostServiceImpl.java
+++ backend/src/main/java/com/social/backend/service/PostServiceImpl.java
@@ -12,18 +12,19 @@ import com.social.backend.exception.NotFoundException;
 import com.social.backend.model.post.Post;
 import com.social.backend.model.user.User;
 import com.social.backend.repository.PostRepository;
+import com.social.backend.util.NullableUtil;
 
 @Service
 @Transactional
 public class PostServiceImpl implements PostService {
-  
+
   private final PostRepository repository;
-  
+
   @Autowired
   public PostServiceImpl(PostRepository repository) {
     this.repository = repository;
   }
-  
+
   @Override
   public Post create(User author, String title, String body) {
     Post entity = new Post();
@@ -32,42 +33,42 @@ public class PostServiceImpl implements PostService {
     entity.setAuthor(author);
     return repository.save(entity);
   }
-  
+
   @Override
   public Post update(Long id, User author, String title, String body) {
     Post entity = findByIdAndAuthor(id, author);
     entity.setUpdatedAt(ZonedDateTime.now());
-    entity.setTitle(title);
-    entity.setBody(body);
+    NullableUtil.set(entity::setTitle, title);
+    NullableUtil.set(entity::setBody, body);
     return repository.save(entity);
   }
-  
+
   @Override
   public void delete(Long id, User author) {
     Post entity = findByIdAndAuthor(id, author);
     repository.delete(entity);
   }
-  
+
   @Override
   public Post find(Long id) {
     return repository.findById(id)
         .orElseThrow(() -> new NotFoundException("notFound.post.byId", id));
   }
-  
+
   @Override
   public Page<Post> findAll(Pageable pageable) {
     return repository.findAll(pageable);
   }
-  
+
   @Override
   public Page<Post> findAll(User author, Pageable pageable) {
     return repository.findAllByAuthor(author, pageable);
   }
-  
+
   private Post findByIdAndAuthor(Long id, User author) {
     return repository.findByIdAndAuthor(id, author)
         .orElseThrow(() -> new NotFoundException(
             "notFound.post.byIdAndAuthor", id, author.getId()));
   }
-  
+
 }
--- backend/src/main/java/com/social/backend/service/UserServiceImpl.java
+++ backend/src/main/java/com/social/backend/service/UserServiceImpl.java
@@ -18,20 +18,21 @@ import com.social.backend.exception.NotFoundException;
 import com.social.backend.exception.WrongCredentialsException;
 import com.social.backend.model.user.User;
 import com.social.backend.repository.UserRepository;
+import com.social.backend.util.NullableUtil;
 
 @Service
 @Transactional
 public class UserServiceImpl implements UserService {
-  
+
   private final UserRepository repository;
   private final PasswordEncoder passwordEncoder;
-  
+
   @Autowired
   public UserServiceImpl(UserRepository repository, PasswordEncoder passwordEncoder) {
     this.repository = repository;
     this.passwordEncoder = passwordEncoder;
   }
-  
+
   @Override
   public User create(String email,
                      String username,
@@ -46,7 +47,7 @@ public class UserServiceImpl implements UserService {
     entity.setPassword(passwordEncoder.encode(password));
     return repository.save(entity);
   }
-  
+
   @Override
   public User update(Long id,
                      String email,
@@ -55,117 +56,115 @@ public class UserServiceImpl implements UserService {
                      String lastName,
                      Integer publicity) {
     User entity = this.find(id);
-    entity.setEmail(email);
-    entity.setUsername(username);
-    entity.setFirstName(firstName);
-    entity.setLastName(lastName);
-    entity.setPublicity(publicity);
+    NullableUtil.set(entity::setEmail, email);
+    NullableUtil.set(entity::setUsername, username);
+    NullableUtil.set(entity::setFirstName, firstName);
+    NullableUtil.set(entity::setLastName, lastName);
+    NullableUtil.set(entity::setPublicity, publicity);
     return repository.save(entity);
   }
-  
+
   @Override
   public User updateRole(Long id, Boolean moder) {
     User entity = this.find(id);
-    entity.setModer(moder);
+    NullableUtil.set(entity::setModer, moder);
     return repository.save(entity);
   }
-  
+
   @Override
-  public void changePassword(Long id,
-                             String actual,
-                             String change) {
+  public void changePassword(Long id, String actual, String change) {
     User entity = this.find(id);
-    
+
     if (!passwordEncoder.matches(actual, entity.getPassword())) {
       throw new WrongCredentialsException("wrongCredentials.password");
     }
-    
+
     entity.setPassword(passwordEncoder.encode(change));
     repository.save(entity);
   }
-  
+
   @Override
   public void delete(Long id, String password) {
     User entity = this.find(id);
-    
+
     if (!passwordEncoder.matches(password, entity.getPassword())) {
       throw new WrongCredentialsException("wrongCredentials.password");
     }
-    
+
     repository.delete(entity);
   }
-  
+
   @Override
   public void addFriend(Long id, Long targetId) {
     if (Objects.equals(id, targetId)) {
       throw new IllegalActionException("illegalAction.user.addHimself");
     }
-    
+
     User entity = this.find(id);
     User target = this.find(targetId);
-    
+
     if (target.isPrivate()) {
       throw new IllegalActionException("illegalAction.user.addPrivate", targetId);
     }
-    
+
     if (entity.hasFriendship(target)) {
       throw new IllegalActionException("illegalAction.user.addPresent", targetId);
     }
-    
+
     entity.setFriends(addFriend(entity, target));
     target.setFriends(addFriend(target, entity));
     repository.save(entity);
     repository.save(target);
   }
-  
+
   @Override
   public void removeFriend(Long id, Long targetId) {
     if (Objects.equals(id, targetId)) {
       throw new IllegalActionException("illegalAction.user.removeHimself", targetId);
     }
-    
+
     User entity = this.find(id);
     User target = this.find(targetId);
-    
+
     if (!entity.hasFriendship(target)) {
       throw new IllegalActionException("illegalAction.user.removeAbsent", targetId);
     }
-    
+
     entity.setFriends(removeFriend(entity, target));
     target.setFriends(removeFriend(target, entity));
     repository.save(entity);
     repository.save(target);
   }
-  
+
   @Override
   public Page<User> getFriends(Long id, Pageable pageable) {
     Set<User> friends = this.find(id).getFriends();
     return new PageImpl<>(new ArrayList<>(friends), pageable, friends.size());
   }
-  
+
   @Override
   public User find(Long id) {
     return repository.findById(id)
         .orElseThrow(() -> new NotFoundException("notFound.user.byId", id));
   }
-  
+
   @Override
   public Page<User> findAll(Pageable pageable) {
     return repository.findAll(pageable);
   }
-  
+
   @SuppressWarnings("checkstyle:OverloadMethodsDeclarationOrder")
   private static Set<User> addFriend(User user, User target) {
     Set<User> friends = new HashSet<>(user.getFriends());
     friends.add(target);
     return friends;
   }
-  
+
   @SuppressWarnings("checkstyle:OverloadMethodsDeclarationOrder")
   private static Set<User> removeFriend(User user, User target) {
     Set<User> friends = new HashSet<>(user.getFriends());
     friends.remove(target);
     return friends;
   }
-  
+
 }
