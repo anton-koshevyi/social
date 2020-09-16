a6b520e15b7d3c9ce5e40aaf9f4be7e0fb50d871
Anton Koshovyi <anton.koshovyi.99@gmail.com>
2020-09-16 22:56:54 +0300

    Implement AbstractMapper

---
+++ backend/src/main/java/com/social/backend/mapper/model/AbstractMapper.java
@@ -0,0 +1,12 @@
+package com.social.backend.mapper.model;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class AbstractMapper<T> {
+
+  protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+  public abstract <R> R toDto(T model, Class<R> dtoType);
+
+}
