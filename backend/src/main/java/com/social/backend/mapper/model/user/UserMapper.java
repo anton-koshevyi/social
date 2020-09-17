d0cdd4eb540faf30356d5ae3d761719eadca5cd8
Anton Koshovyi <anton.koshovyi.99@gmail.com>
2020-09-17 11:18:16 +0300

    Implement User model mapper

--- backend/src/main/java/com/social/backend/mapper/model/user/UserMapper.java
+++ backend/src/main/java/com/social/backend/mapper/model/user/UserMapper.java
@@ -1,9 +1,5 @@
-package com.social.backend.adapter.json;
+package com.social.backend.mapper.model.user;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.jackson.JsonComponent;
 import org.springframework.security.access.expression.SecurityExpressionRoot;
 import org.springframework.security.authentication.AuthenticationTrustResolverImpl;
 import org.springframework.security.core.Authentication;
@@ -11,69 +7,82 @@ import org.springframework.security.core.context.SecurityContextHolder;
 
 import com.social.backend.config.IdentifiedUserDetails;
 import com.social.backend.config.SecurityConfig.Authority;
-import com.social.backend.dto.EntityMapper;
 import com.social.backend.dto.user.UserDto;
+import com.social.backend.mapper.model.AbstractMapper;
 import com.social.backend.model.user.User;
 import com.social.backend.util.AuthenticationUtil;
 
-@JsonComponent
-public class UserSerializer extends AbstractSerializer<User> {
-  
-  private static final Logger logger = LoggerFactory.getLogger(UserSerializer.class);
-  
-  private final EntityMapper<User, UserDto> entityMapper;
-  
-  @Autowired
-  public UserSerializer(EntityMapper<User, UserDto> entityMapper) {
-    this.entityMapper = entityMapper;
-  }
-  
+public class UserMapper extends AbstractMapper<User> {
+
   @SuppressWarnings({"checkstyle:CyclomaticComplexity", "checkstyle:JavaNCSS"})
   @Override
-  public Object beforeSerialize(User user) {
+  public <R> R toDto(User model, Class<R> dtoType) {
     Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-    
+
     if (authentication == null) {
-      if (user.isPublic()) {
+      if (model.isPublic()) {
         // Unable to check request from administration
         logger.debug("Null authentication - regular 'public' body");
-        return entityMapper.map(user);
+        return regular(model, dtoType);
       }
-      
+
       logger.debug("Null authentication - hidden body");
-      return entityMapper.mapHidden(user);
+      return hidden(model, dtoType);
     }
-    
+
     SecurityExpressionRoot security = new SecurityExpressionRoot(authentication) {};
     security.setTrustResolver(new AuthenticationTrustResolverImpl());
-    
+
     if (security.hasAnyAuthority(Authority.MODER, Authority.ADMIN)) {
       // Administration role-based access logic will be here.
       // E.g. Moder cannot get extended data of another moder.
       logger.debug("Administration principal - extended body");
-      return entityMapper.mapExtended(user);
+      return regular(model, dtoType);
     }
-    
+
     IdentifiedUserDetails principal = AuthenticationUtil.getPrincipal(authentication);
-    
-    if (principal != null && principal.getId().equals(user.getId())) {
+
+    if (principal != null && principal.getId().equals(model.getId())) {
       logger.debug("Owner principal - regular body");
-      return entityMapper.map(user);
+      return regular(model, dtoType);
     }
-    
-    if (user.isPublic()) {
+
+    if (model.isPublic()) {
       // Request is not from administration
-      logger.debug("Regular 'public' body");
-      return entityMapper.map(user);
+      logger.debug("Authenticated - regular 'public' body");
+      return regular(model, dtoType);
     }
-    
-    if (user.isInternal() && security.isAuthenticated()) {
+
+    // TODO: Authenticated check duplication
+
+    if (model.isInternal() && security.isAuthenticated()) {
       logger.debug("Authenticated - regular 'internal' body");
-      return entityMapper.map(user);
+      return regular(model, dtoType);
     }
-    
+
     logger.debug("Hidden body by default");
-    return entityMapper.mapHidden(user);
+    return hidden(model, dtoType);
   }
-  
+
+  public <R> R regular(User source, Class<R> dtoType) {
+    logger.debug("Mapping User to regular UserDto by default");
+    UserDto dto = new UserDto();
+    dto.setId(source.getId());
+    dto.setEmail(source.getEmail());
+    dto.setUsername(source.getUsername());
+    dto.setFirstName(source.getFirstName());
+    dto.setLastName(source.getLastName());
+    dto.setPublicity(source.getPublicity());
+    dto.setModer(source.isModer());
+    dto.setAdmin(source.isAdmin());
+    return (R) dto;
+  }
+
+  public <R> R hidden(User source, Class<R> dtoType) {
+    logger.debug("Mapping User to hidden UserDto by default");
+    UserDto dto = regular(source, UserDto.class);
+    dto.setEmail(null);
+    return (R) dto;
+  }
+
 }
---
+++ backend/src/test/java/com/social/backend/mapper/model/user/UserMapperTest.java
@@ -0,0 +1,268 @@
+package com.social.backend.mapper.model.user;
+
+import java.util.Collections;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.TestingAuthenticationProvider;
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.social.backend.TestEntity;
+import com.social.backend.config.IdentifiedUserDetails;
+import com.social.backend.config.SecurityConfig.Authority;
+import com.social.backend.dto.user.UserDto;
+import com.social.backend.model.user.Publicity;
+import com.social.backend.model.user.User;
+
+@ExtendWith(SpringExtension.class)
+public class UserMapperTest {
+
+  private final UserMapper userMapper = new UserMapper();
+
+  @Test
+  public void given_notPublicPublicity_when_nullAuthentication_then_hiddenBody() {
+    User user = TestEntity
+        .user()
+        .setId(1L)
+        .setPublicity(Publicity.INTERNAL);
+
+    Assertions
+        .assertThat(userMapper.toDto(user, UserDto.class))
+        .usingRecursiveComparison()
+        .isEqualTo(new UserDto()
+            .setId(1L)
+            .setEmail(null)
+            .setUsername("username")
+            .setFirstName("first")
+            .setLastName("last")
+            .setPublicity(Publicity.INTERNAL)
+            .setModer(false)
+            .setAdmin(false));
+  }
+
+  @Test
+  public void given_publicPublicity_when_nullAuthentication_then_regularBody() {
+    User user = TestEntity
+        .user()
+        .setId(1L)
+        .setPublicity(Publicity.PUBLIC);
+
+    Assertions
+        .assertThat(userMapper.toDto(user, UserDto.class))
+        .usingRecursiveComparison()
+        .isEqualTo(new UserDto()
+            .setId(1L)
+            .setEmail("email@mail.com")
+            .setUsername("username")
+            .setFirstName("first")
+            .setLastName("last")
+            .setPublicity(Publicity.PUBLIC)
+            .setModer(false)
+            .setAdmin(false));
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {Publicity.PUBLIC, Publicity.INTERNAL, Publicity.PRIVATE})
+  @WithMockUser(authorities = Authority.MODER)
+  public void given_anyPublicity_when_administrationRequest_then_extendedBody(int publicity) {
+    User user = TestEntity
+        .user()
+        .setId(1L)
+        .setPublicity(publicity);
+
+    Assertions
+        .assertThat(userMapper.toDto(user, UserDto.class))
+        .usingRecursiveComparison()
+        .isEqualTo(new UserDto()
+            .setId(1L)
+            .setEmail("email@mail.com")
+            .setUsername("username")
+            .setFirstName("first")
+            .setLastName("last")
+            .setPublicity(publicity)
+            .setModer(false)
+            .setAdmin(false));
+  }
+
+  @Test
+  @WithMockUser
+  public void given_privatePublicity_when_notIdentifiedPrincipal_then_hiddenBody() {
+    User user = TestEntity
+        .user()
+        .setId(1L)
+        .setPublicity(Publicity.PRIVATE);
+
+    Assertions
+        .assertThat(userMapper.toDto(user, UserDto.class))
+        .usingRecursiveComparison()
+        .isEqualTo(new UserDto()
+            .setId(1L)
+            .setEmail(null)
+            .setUsername("username")
+            .setFirstName("first")
+            .setLastName("last")
+            .setPublicity(Publicity.PRIVATE)
+            .setModer(false)
+            .setAdmin(false));
+  }
+
+  @Test
+  public void given_privatePublicity_when_notOwnerRequest_then_hiddenBody() {
+    authenticate(new IdentifiedUserDetails(
+        2L,
+        "username",
+        "password",
+        Collections.emptySet()
+    ));
+    User user = TestEntity
+        .user()
+        .setId(1L)
+        .setPublicity(Publicity.PRIVATE);
+
+    Assertions
+        .assertThat(userMapper.toDto(user, UserDto.class))
+        .usingRecursiveComparison()
+        .isEqualTo(new UserDto()
+            .setId(1L)
+            .setEmail(null)
+            .setUsername("username")
+            .setFirstName("first")
+            .setLastName("last")
+            .setPublicity(Publicity.PRIVATE)
+            .setModer(false)
+            .setAdmin(false));
+  }
+
+  @Test
+  public void given_anyPublicity_when_ownerRequest_then_regularBody() {
+    authenticate(new IdentifiedUserDetails(
+        1L,
+        "username",
+        "password",
+        Collections.emptySet()
+    ));
+    User user = TestEntity
+        .user()
+        .setId(1L)
+        .setPublicity(Publicity.PRIVATE);
+
+    Assertions
+        .assertThat(userMapper.toDto(user, UserDto.class))
+        .usingRecursiveComparison()
+        .isEqualTo(new UserDto()
+            .setId(1L)
+            .setEmail("email@mail.com")
+            .setUsername("username")
+            .setFirstName("first")
+            .setLastName("last")
+            .setPublicity(Publicity.PRIVATE)
+            .setModer(false)
+            .setAdmin(false));
+  }
+
+  @Test
+  @WithAnonymousUser
+  public void given_publicPublicity_when_notOwnerNorAdministrationRequest_then_regularBody() {
+    User user = TestEntity
+        .user()
+        .setId(1L)
+        .setPublicity(Publicity.PUBLIC);
+
+    Assertions
+        .assertThat(userMapper.toDto(user, UserDto.class))
+        .usingRecursiveComparison()
+        .isEqualTo(new UserDto()
+            .setId(1L)
+            .setEmail("email@mail.com")
+            .setUsername("username")
+            .setFirstName("first")
+            .setLastName("last")
+            .setPublicity(Publicity.PUBLIC)
+            .setModer(false)
+            .setAdmin(false));
+  }
+
+  @Test
+  @WithAnonymousUser
+  public void given_internalPublicity_when_anonymousRequest_then_hiddenBody() {
+    User user = TestEntity
+        .user()
+        .setId(1L)
+        .setPublicity(Publicity.INTERNAL);
+
+    Assertions
+        .assertThat(userMapper.toDto(user, UserDto.class))
+        .usingRecursiveComparison()
+        .isEqualTo(new UserDto()
+            .setId(1L)
+            .setEmail(null)
+            .setUsername("username")
+            .setFirstName("first")
+            .setLastName("last")
+            .setPublicity(Publicity.INTERNAL)
+            .setModer(false)
+            .setAdmin(false));
+  }
+
+  @Test
+  @WithMockUser
+  public void given_internalPublicity_when_authenticatedRequest_then_regularBody() {
+    User user = TestEntity
+        .user()
+        .setId(1L)
+        .setPublicity(Publicity.INTERNAL);
+
+    Assertions
+        .assertThat(userMapper.toDto(user, UserDto.class))
+        .usingRecursiveComparison()
+        .isEqualTo(new UserDto()
+            .setId(1L)
+            .setEmail("email@mail.com")
+            .setUsername("username")
+            .setFirstName("first")
+            .setLastName("last")
+            .setPublicity(Publicity.INTERNAL)
+            .setModer(false)
+            .setAdmin(false));
+  }
+
+  @Test
+  @WithMockUser
+  public void given_privatePublicity_when_notOwnerNorAdministrationRequest_hiddenBody() {
+    User user = TestEntity
+        .user()
+        .setId(1L)
+        .setPublicity(Publicity.PRIVATE);
+
+    Assertions
+        .assertThat(userMapper.toDto(user, UserDto.class))
+        .usingRecursiveComparison()
+        .isEqualTo(new UserDto()
+            .setId(1L)
+            .setEmail(null)
+            .setUsername("username")
+            .setFirstName("first")
+            .setLastName("last")
+            .setPublicity(Publicity.PRIVATE)
+            .setModer(false)
+            .setAdmin(false));
+  }
+
+  private static void authenticate(UserDetails details) {
+    AuthenticationProvider provider = new TestingAuthenticationProvider();
+    Authentication token = new TestingAuthenticationToken(details, details.getPassword());
+    Authentication authentication = provider.authenticate(token);
+    SecurityContextHolder.getContext().setAuthentication(authentication);
+  }
+
+}
