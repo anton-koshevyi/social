a24d921bc712d95a0f2025b2c044c6b92a1116b2
Anton Koshovyi <anton.koshovyi.99@gmail.com>
2020-09-20 22:42:25 +0300

    Remove dependency-management plugin

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -2,8 +2,7 @@ plugins {
   java
   jacoco
   checkstyle
-  id("org.springframework.boot") version "2.2.0.RELEASE"
-  id("io.spring.dependency-management") version "1.0.9.RELEASE"
+  id("org.springframework.boot").version("2.2.0.RELEASE")
 }
 
 repositories {
@@ -23,11 +22,11 @@ dependencies {
   testImplementation("io.rest-assured:rest-assured:4.2.0")
   testImplementation("io.rest-assured:spring-mock-mvc:4.2.0")
 
-  implementation("org.springframework.boot:spring-boot-starter-actuator")
-  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-  implementation("org.springframework.boot:spring-boot-starter-security")
-  implementation("org.springframework.boot:spring-boot-starter-web")
-  testImplementation("org.springframework.boot:spring-boot-starter-test")
+  implementation("org.springframework.boot:spring-boot-starter-actuator:2.2.0.RELEASE")
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.2.0.RELEASE")
+  implementation("org.springframework.boot:spring-boot-starter-security:2.2.0.RELEASE")
+  implementation("org.springframework.boot:spring-boot-starter-web:2.2.0.RELEASE")
+  testImplementation("org.springframework.boot:spring-boot-starter-test:2.2.0.RELEASE")
 }
 
 java {
