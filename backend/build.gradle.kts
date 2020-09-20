1de4c40bef7d5b41908a0394034829f42eed6e69
Anton Koshovyi <anton.koshovyi.99@gmail.com>
2020-09-20 23:05:38 +0300

    Optimize main dependencies

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -10,19 +10,46 @@ repositories {
 }
 
 dependencies {
+
+  // CDI
+  implementation("org.springframework:spring-context:5.2.0.RELEASE")
+  implementation("org.springframework.boot:spring-boot-autoconfigure:2.2.0.RELEASE")
+
+  // Common
   implementation("com.google.guava:guava:29.0-jre")
-  implementation("com.h2database:h2:1.4.198")
   implementation("org.apache.commons:commons-lang3:3.9")
-  implementation("org.flywaydb:flyway-core:6.0.8")
   implementation("org.mapstruct:mapstruct:1.3.1.Final")
   annotationProcessor("org.mapstruct:mapstruct-processor:1.3.1.Final")
-  implementation("org.postgresql:postgresql:42.2.5")
   implementation("org.projectlombok:lombok:1.18.12")
   annotationProcessor("org.projectlombok:lombok:1.18.12")
+  implementation("org.yaml:snakeyaml:1.25")
 
-  implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.2.0.RELEASE")
-  implementation("org.springframework.boot:spring-boot-starter-security:2.2.0.RELEASE")
-  implementation("org.springframework.boot:spring-boot-starter-web:2.2.0.RELEASE")
+  // Data
+  implementation("com.h2database:h2:1.4.197")
+  implementation("com.zaxxer:HikariCP:3.4.1")
+  implementation("org.flywaydb:flyway-core:6.0.8")
+  implementation("org.hibernate:hibernate-core:5.4.10.Final")
+  implementation("org.postgresql:postgresql:42.2.5")
+  implementation("org.springframework.data:spring-data-jpa:2.2.0.RELEASE")
+
+  // Logging
+  implementation("ch.qos.logback:logback-classic:1.2.3")
+  implementation("org.slf4j:jul-to-slf4j:1.7.30")
+  implementation("org.slf4j:slf4j-api:1.7.30")
+
+  // Security
+  implementation("org.springframework.security:spring-security-config:5.2.0.RELEASE")
+  implementation("org.springframework.security:spring-security-crypto:5.2.0.RELEASE")
+  implementation("org.springframework.security:spring-security-web:5.2.0.RELEASE")
+
+  // Validation
+  implementation("org.glassfish:javax.el:3.0.0")
+  implementation("org.hibernate.validator:hibernate-validator:6.1.5.Final")
+
+  // Web
+  implementation("com.fasterxml.jackson.core:jackson-databind:2.11.0")
+  implementation("org.apache.tomcat.embed:tomcat-embed-core:9.0.37")
+  implementation("org.springframework:spring-webmvc:5.2.0.RELEASE")
 
 
   // Test: Asserting
