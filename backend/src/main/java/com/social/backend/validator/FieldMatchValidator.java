b68bba9a67135294927f097c1c8b2ce73df705ca
Anton Koshovyi <anton.koshovyi.99@gmail.com>
2020-09-21 10:50:45 +0300

    Test case where FieldUtils::readField throws exception

--- backend/src/main/java/com/social/backend/validator/FieldMatchValidator.java
+++ backend/src/main/java/com/social/backend/validator/FieldMatchValidator.java
@@ -9,24 +9,24 @@ import org.apache.commons.lang3.reflect.FieldUtils;
 import com.social.backend.constraint.FieldMatch;
 
 public class FieldMatchValidator implements ConstraintValidator<FieldMatch, Object> {
-  
+
   private String field;
   private String compared;
   private boolean notMatch;
-  
+
   @Override
   public void initialize(FieldMatch annotation) {
     this.field = annotation.field();
     this.compared = annotation.compared();
     this.notMatch = annotation.notMatch();
   }
-  
+
   @Override
   public boolean isValid(Object source, ConstraintValidatorContext context) {
     Object fieldValue = readField(source, field);
     Object comparedValue = readField(source, compared);
     boolean valid = (notMatch != Objects.equals(fieldValue, comparedValue));
-    
+
     if (!valid) {
       String message = context.getDefaultConstraintMessageTemplate();
       context.buildConstraintViolationWithTemplate(message)
@@ -34,18 +34,16 @@ public class FieldMatchValidator implements ConstraintValidator<FieldMatch, Obje
           .addConstraintViolation()
           .disableDefaultConstraintViolation();
     }
-    
+
     return valid;
   }
-  
+
   private static Object readField(Object source, String field) {
     try {
       return FieldUtils.readField(source, field, true);
     } catch (IllegalAccessException e) {
-      throw new RuntimeException("Unable to read"
-          + " field: " + field
-          + ", source: " + source, e);
+      throw new RuntimeException("Unable to read field: " + field, e);
     }
   }
-  
+
 }
