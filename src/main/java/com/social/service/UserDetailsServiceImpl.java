9ad30fdb3b1ef10cf66cb6c65cece170f1714add
Anton Koshovyi <anton.koshovyi.99@gmail.com>
2021-01-09 20:03:55 +0200

    Move authorities collecting to method

--- src/main/java/com/social/service/UserDetailsServiceImpl.java
+++ src/main/java/com/social/service/UserDetailsServiceImpl.java
@@ -1,10 +1,12 @@
 package com.social.service;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -18,38 +20,42 @@ import com.social.repository.UserRepository;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
-  
+
   private final UserRepository userRepository;
-  
+
   @Autowired
   public UserDetailsServiceImpl(UserRepository userRepository) {
     this.userRepository = userRepository;
   }
-  
+
   @Override
   public UserDetails loadUserByUsername(String username) {
     User entity = userRepository.findByEmail(username)
         .orElseGet(() -> userRepository.findByUsername(username)
             .orElseThrow(() -> new UsernameNotFoundException(
                 "No user with email or username: " + username)));
-    Set<String> authorities = new HashSet<>();
-    
-    if (entity.isModer()) {
-      authorities.add(Authority.MODER);
-    }
-    
-    if (entity.isAdmin()) {
-      authorities.add(Authority.ADMIN);
-    }
-    
     return new IdentifiedUserDetails(
         entity.getId(),
         username,
         entity.getPassword(),
-        authorities.stream()
-            .map(SimpleGrantedAuthority::new)
-            .collect(Collectors.toSet())
+        collectAuthorities(entity)
     );
   }
-  
+
+  private static Collection<GrantedAuthority> collectAuthorities(User entity) {
+    Set<String> authorities = new HashSet<>();
+
+    if (entity.isModer()) {
+      authorities.add(Authority.MODER);
+    }
+
+    if (entity.isAdmin()) {
+      authorities.add(Authority.ADMIN);
+    }
+
+    return authorities.stream()
+        .map(SimpleGrantedAuthority::new)
+        .collect(Collectors.toSet());
+  }
+
 }
